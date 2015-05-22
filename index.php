a47dca1503fe3f3bbe94d9d71d01679dd81587dd
Angela Banuelos <banuelosangela56@gmail.com>
2015-05-21 20:31:41 -0700

    Created pages

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="main.css">
+	<title></title>
+</head>
+<body>
+<div id="bar1">	
+<div id="letter">
+	A
+</div>
+</div>
+<!-- <div id="sidebar1">	
+</div>
+<div id="sidebar2">
+</div> -->
+
+</body>
+</html>
