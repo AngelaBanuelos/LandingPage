a4ac613ed5a434c35f703896d35bb4e4fb0eb605
Angela Banuelos <banuelosangela56@gmail.com>
2015-05-22 11:21:55 -0700

    Stylized home page

--- index.php
+++ index.php
@@ -6,14 +6,34 @@
 </head>
 <body>
 <div id="bar1">	
-<div id="letter">
+	<div id="letter">
 	A
+	</div>
+<div id="la">
+	<hr>
+		<span>
+	</hr>	
+			Los Angeles, California
+	<hr>		
+		</span>
+	</hr>
 </div>
+<div class="circular"></div>
+<footer id="footer">
+	<div class="soc">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<ul class="socials">
+ 
+  <li id="f"><a href="http://facebook.com"><i class="fa fa-facebook"></a></i></li>
+  <li><a href="http://mail.google.com"><i class="fa fa-google"></a></i></li>
+  <li id="t"><a href="http://twitter.com"><i class="fa fa-twitter"></a></i></li>
+  <li><a href="http://youtube.com"><i class="fa fa-youtube"></a></i></li>
+<!--  add anchor tags, and off u go -->
+  
+    </ul> 
 </div>
-<!-- <div id="sidebar1">	
-</div>
-<div id="sidebar2">
-</div> -->
 
+</footer>
+</div>
 </body>
 </html>
